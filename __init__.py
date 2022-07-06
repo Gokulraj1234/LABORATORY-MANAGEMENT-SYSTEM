d24cbd6b6b39ea7a6ed4fc51d225e70f36b66d22
Gokulraj1234 <76981070+Gokulraj1234@users.noreply.github.com>
2022-07-06 20:09:18 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#
+#
+#############################################################################
+
+from . import physician_speciality
+from . import res_partner
+from . import lab_patient
+from . import testing_unit
+from . import lab_test_type
+from . import lab_test_content_type
+from . import lab_appointment
+from . import lab_request
+from . import account_invoice
+
+
